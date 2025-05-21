2de8cc523159220ce122f0cc85f3d623b011039f
RJP5546 <111258984+RJP5546@users.noreply.github.com>
2025-05-20 21:14:10 -0500

    new test scene created and verified working with multiple input devices

--- MultiplayerFirstTestProject/Assets/Scripts/PlayerMovement.cs
+++ MultiplayerFirstTestProject/Assets/Scripts/PlayerMovement.cs
@@ -13,6 +13,22 @@ public class PlayerMovement : NetworkBehaviour
     [SerializeField]
     Camera playerCamera;
 
+    [SerializeField]
+    bool spawnPlayer = false;
+
+    private void Start()
+    {
+        if (!GetComponent<NetworkObject>().IsSpawned)
+        {
+            GetComponent<NetworkObject>().Spawn();
+        }
+    }
+
+    private void Update()
+    {
+        if (spawnPlayer) { GetComponent<NetworkObject>().Spawn(); spawnPlayer = false; }
+    }
+
     public override void OnNetworkSpawn()
     {
         base.OnNetworkSpawn();
@@ -33,7 +49,7 @@ public class PlayerMovement : NetworkBehaviour
         // position on owning clients
         m_CharacterController.enabled = true;
 
-        playerCamera.enabled = true;
+        //playerCamera.enabled = true;
 
     }
 
